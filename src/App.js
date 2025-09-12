2a72937685f4e809e31e5fa2c9fda12ff0846c8d
Awishka0573 <awishkaisuru0573@gmail.com>
2025-09-12 11:58:20 +0530

    wrap using fragment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div>
-      
+      AIK Learning React Course
+      <h1>Awishka</h1>
+
     </div>
+    <h2>Awishka Isuru</h2>
+    </>
   );
 }
 
