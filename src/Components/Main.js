a3124be22a11a4a7c1245ef823793eb100910166
Awishka0573 <awishkaisuru0573@gmail.com>
2025-09-12 21:54:41 +0530

    create object and call it

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,13 +1,22 @@
 function Main(props) {
     const name = "Awishka Isuru";
     const position = "developer";
+
+
+    const myObj ={
+        name : `{name}`,
+        position : `{position}`,
+        city : `Kegalle`
+    }
+
+    const {city} = myObj;
     return (
         <>
         <div>This is Main Component</div>
         <ul>
             <li>{name}</li>
             <li>{position}</li>
-            <li>Item 3</li>
+            <li>{city}</li>
             <li>Item 4</li>
         </ul>
         {props.children}
