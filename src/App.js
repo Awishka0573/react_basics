ece71106c03490292dcce87119440b09539b7027
Awishka0573 <awishkaisuru0573@gmail.com>
2025-09-16 12:28:32 +0530

    create internal component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import './App.css';
 import Main from './Components/Main';
-import myData from './Data/myData';
+import {myData} from './Data/myData';
+import {myData2} from './Data/myData';
+
+
+const myDatain =myData.map(({name,city,position,age,id})=>{
+        return <Main key={id} name={name} position={position} city={city} age={age} />
+      })
+
 
 
 function App() {
@@ -12,10 +19,7 @@ function App() {
 
     </div>
     <h2>Awishka Isuru</h2>
-    {
-      myData.map(({name,city,position,age,id})=>{
-        return <Main key={id} name={name} position={position} city={city} age={age} />
-      })
+    { myDatain 
     }
    
     </>
--- src/Data/myData.js
+++ src/Data/myData.js
@@ -29,4 +29,37 @@ const myData = [
   },
 ];
 
-export default myData;
+export  {myData};
+
+const myData2 = [
+  {
+    id:1,
+  name : 'Awishka ',
+  position : 'developer',
+  city : 'Kegalle',
+  age : '22',
+  },
+  {
+     id:12,
+  name : 'sandesh',
+  position : 'developer',
+  city : 'Kegalle',
+  age : '22',
+  },
+  {
+     id:123,
+  name : 'Ahinsa ',
+  position : 'developer',
+  city : 'Mathara',
+  age : '22',
+  },
+  {
+     id:1234,
+  name : 'Isuru ',
+  position : 'developer',
+  city : 'Mathara',
+  age : '22',
+  },
+];
+
+export { myData2};
